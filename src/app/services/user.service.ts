5b2f964bfad2d090c9e1bbb08f31aa1bd6fe3038
Afham Shahrin <afham.shahrin@aemenersol.com>
2022-07-30 22:04:17 +0800

    integrate electron on existing angular project

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,16 +1,15 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
-
+  baseUrl = "http://test-demo.aemenersol.com/api"
   constructor(private http:HttpClient) { }
 
   public getData(): Observable<any> {
-    return this.http.get<any>(`${environment.baseUrl}/dashboard`);
+    return this.http.get<any>(`${this.baseUrl}/dashboard`);
 }
 }
